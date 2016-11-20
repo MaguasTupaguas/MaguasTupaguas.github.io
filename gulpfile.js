400063ac7d2d02776f0a4c45acb65cb3ea2aa6b8
MaguasTupaguas <mauaznar94@hotmail.com>
2016-11-19 18:27:46 -0600

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var jshint = require('gulp-jshint');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
-var sourcemaps = require('gulp-sourcemaps');
+//var sourcemaps = require('gulp-sourcemaps');
 var notify = require('gulp-notify');
 var autoprefixer = require('gulp-autoprefixer');
 var htmlmin = require('gulp-htmlmin');
@@ -13,7 +13,11 @@ var uglify = require('gulp-uglify');
 var del = require('del');
 var plumber = require('gulp-plumber');
 var cp = require('child_process');
+
 var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
 
 gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
@@ -21,52 +25,54 @@ gulp.task('jekyll-build', function (done) {
         .on('close', done);
 });
 
-gulp.task('js', ['jsdel'], function(){
-  return gulp.src('./app/src/js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./app/assets/js'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
 });
 
-gulp.task('browserSync', function(){
+
+
+gulp.task('browserSync', ['jekyll-build'] , function(){
   browserSync.init({
     server: {
       baseDir: '_site'
-    }
+    },
+    notify: false
   });
 });
 
 gulp.task('sass', function(){
-    return gulp.src('./app/src/sass/main.scss')
-      .pipe(sourcemaps.init())
-      .pipe(sass()
-        .on('error', sass.logError)
-       )
+    return gulp.src('assets/css/main.scss')
+      .pipe(sass({
+          includePaths: ['css'],
+          onError: browserSync.notify
+      }))
       .pipe(autoprefixer())
-      //.pipe(minifyCss())
-      .pipe(gulp.dest('./app/assets/css'))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./app/assets/maps'))
+      .pipe(minifyCss())
+      .pipe(gulp.dest('_site/assets/css'))
+      .pipe(browserSync.reload({stream:true}))
+      .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('html', function(){
+    return gulp.src('_jadefiles/*.jade')
+      .pipe(jade())
+      .pipe(gulp.dest('_includes'))
       .pipe(browserSync.reload({
         stream: true
       }));
 });
 
-gulp.task('html', function(){
-    return gulp.src('./app/src/jade/*.jade')
-      .pipe(plumber())
-      .pipe(jade())
-      .pipe(htmlmin())
-      .pipe(plumber.stop())
-      .pipe(gulp.dest('./app'))
-      .pipe(browserSync.reload({
-        stream: true
-      }));
+
+gulp.task('js', ['jsdel'], function(){
+  return gulp.src('assets/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('functions.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('_site/assets/js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 gulp.task('jsdel', function(){
@@ -75,9 +81,11 @@ gulp.task('jsdel', function(){
   ]);
 });
 
-gulp.task("default", ['html', 'js', 'sass','browserSync'], function(){
-  gulp.watch('./app/src/jade/*.jade', ["html"]);
-  gulp.watch('./app/src/js/*.js', ["js"]);
-  gulp.watch('./app/src/sass/**/*.scss', ["sass"]);
+
+gulp.task("default", ['html', 'sass', 'js', 'browserSync'], function(){
+  gulp.watch('assets/css/**', ['sass']);
+  gulp.watch('assets/js/*.js', ['js']);
+  gulp.watch('_jadefiles/*.jade', ["html"]);
+  gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
 
 });
